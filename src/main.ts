0de703dc33c9835b9df51cc358e612d5f51b3b1f
Acidiney Dias <24493328+acidiney@users.noreply.github.com>
2025-11-16 12:48:05 +0000

    feat(admin): add admin panel with record and order management

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { AppConfig } from './app.config';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; // Import Swagger
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // Import Swagger
+import { join } from 'path';
+import { AppConfig } from './app.config';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setBaseViewsDir(join(__dirname, 'views'));
+  app.setViewEngine('hbs');
 
   app.useGlobalPipes(
     new ValidationPipe({
