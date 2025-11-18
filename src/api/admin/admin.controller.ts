59b566b5bdfec03af193d5ea53d88e3b0f125863
Acidiney Dias <24493328+acidiney@users.noreply.github.com>
2025-11-18 18:34:00 +0000

    refactor: remove static code on template

--- src/api/admin/admin.controller.ts
+++ src/api/admin/admin.controller.ts
@@ -11,7 +11,13 @@ export class AdminController {
     return Sentry.startSpan(
       { name: 'AdminController#index', op: 'controller' },
       async () => {
-        return {};
+        const allowedDomains =
+          process.env.SENTRY_ALLOWED_DOMAINS?.split(',') || [];
+        return {
+          SENTRY_DNS: process.env.SENTRY_DNS,
+          SENTRY_ALLOWED_DOMAINS: allowedDomains,
+          SENTRY_FE_HASH_SCRIPT: process.env.SENTRY_FE_HASH_SCRIPT,
+        };
       },
     );
   }
